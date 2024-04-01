3b5ba23ffc22885b970c1dee02855be48ded367c
name <name@gmail.com>
2024-04-01 16:27:20 -0400

    UI

--- Mood Hacker/ContentView.swift
+++ Mood Hacker/ContentView.swift
@@ -1,61 +1,115 @@
 //
 //  ContentView.swift
-//  Mood Hacker
+//  MoodHacker
 //
-//  Created by Rachel Leiner on 2/27/24.
+//  Created by Samantha Amaro on 3/29/24.
 //
 
 import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
+        signUpPage()
     }
+}
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
+
+struct ContentView_Previews: PreviewProvider{
+    static var previews: some View {
+        ContentView()
     }
+}
 
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
+struct signUpBtn: View {
+    var body: some View{
+        Text("Sign in").frame(maxWidth:200).padding().font(.title3).background(Color(red: 0.7, green:0.4, blue: 1)).foregroundColor(Color.white).cornerRadius(50)
+    }
+}
+
+struct logInbtn: View {
+    var body: some View{
+        Text("Log in").frame(maxWidth:200).padding().font(.title3).background(Color(red: 0.7, green:0.4, blue: 1)).foregroundColor(Color.white).cornerRadius(50)
+    }
+}
+
+
+
+struct signUpPage: View{
+    @State private var username : String = ""
+    @State private var email : String = ""
+    @State private var password : String = ""
+    var body: some View{
+        NavigationView{
+            
+        
+            ZStack{
+                Color(hue: 0.785, saturation: 0.096, brightness: 0.996).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                
+                VStack{
+                    Image("Image").resizable().aspectRatio(contentMode:.fit).frame(width:200, height: 150)
+                    Text("Mood Hacker").font(.title).fontWeight(.bold).padding(.all)
+                    Text("create a new account").foregroundColor(/*@START_MENU_TOKEN@*/Color(red: 0.704, green: 0.401, blue: 1.001)/*@END_MENU_TOKEN@*/).padding(.bottom)
+                    
+                    TextField("Username", text: $username).padding(.all, 10).frame(maxWidth:290).font(.title3).background(Color.white).cornerRadius(20).padding(.all)
+                    
+                    TextField("Email", text: $email).padding(.all, 10).frame(maxWidth:290).font(.title3).background(Color.white).cornerRadius(20).padding(.bottom)
+                    
+                    TextField("Password", text: $password).padding(.all, 10).frame(maxWidth:290).font(.title3).background(Color.white).cornerRadius(20).padding(.bottom)
+                    
+                    TextField("Confirm Password", text: $password).padding(.all, 10).frame(maxWidth:290).font(.title3).background(Color.white).cornerRadius(20).padding(.bottom)
+                    
+                    
+                    signUpBtn().padding(.top)
+                    
+                    HStack{
+                        
+                        Text("Already have an account?").foregroundColor(.black)
+                        
+                        NavigationLink(destination: logInScreen(), label:{
+                            Text("Login").foregroundColor(Color(red: 0.7, green:0.4, blue: 1))
+                        }).navigationBarHidden(true).fontWeight(.bold)
+                        
+                    }.padding(.top)
+                }
+            }
+            
+        }
+        
+    }
+}
+
+struct logInScreen : View{
+    @State private var username : String = ""
+    @State private var email : String = ""
+    @State private var password : String = ""
+    var body: some View{
+        
+        ZStack{
+            Color(hue: 0.785, saturation: 0.096, brightness: 0.996).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack{
+                Image("Image").resizable().aspectRatio(contentMode:.fit).frame(width:200, height: 150)
+                Text("Mood Hacker").font(.title).fontWeight(.bold).padding(.all)
+                Text("create a new account").foregroundColor(/*@START_MENU_TOKEN@*/Color(red: 0.704, green: 0.401, blue: 1.001)/*@END_MENU_TOKEN@*/).padding(.bottom)
+                
+                TextField("Username", text: $username).padding(.all, 10).frame(maxWidth:290).font(.title3).background(Color.white).cornerRadius(20).padding(.all)
+                
+                TextField("Password", text: $password).padding(.all, 10).frame(maxWidth:290).font(.title3).background(Color.white).cornerRadius(20).padding(.bottom)
+                
+                
+                logInbtn().padding(.top)
+                
+                HStack{
+                    
+                    Text("New user?").foregroundColor(.black)
+                    
+                    NavigationLink(destination: ContentView().navigationBarBackButtonHidden(true), label:{
+                        Text("Sign Up").foregroundColor(Color(red: 0.7, green:0.4, blue: 1))
+                    }).navigationBarHidden(true).fontWeight(.bold)
+                    
+                }.padding(.top)
             }
         }
     }
 }
 
-#Preview {
-    ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
-}
--- Mood Hacker/Mood_HackerApp.swift
+++ Mood Hacker/Mood_HackerApp.swift
@@ -1,32 +1,18 @@
 //
-//  Mood_HackerApp.swift
-//  Mood Hacker
+//  MoodHackerApp.swift
+//  MoodHacker
 //
-//  Created by Gabrielle Pariag on 2/27/24.
+//  Created by Samantha Amaro on 3/29/24.
 //
 
 import SwiftUI
 import SwiftData
 
 @main
-struct Mood_HackerApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
-    var body: some Scene {
-        WindowGroup {
+struct MoodHackerApp: App {
+    var body: some Scene{
+        WindowGroup{
             ContentView()
         }
-        .modelContainer(sharedModelContainer)
     }
 }
