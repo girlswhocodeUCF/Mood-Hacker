9661e14dfbb076327b6d17ffe26401189d008246
name <name@gmail.com>
2024-04-01 16:38:47 -0400

    Initial Commit

---
+++ MoodHacker/Mood_HackerApp.swift
@@ -0,0 +1,18 @@
+//
+//  MoodHackerApp.swift
+//  MoodHacker
+//
+//  Created by Samantha Amaro on 3/29/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Mood_HackerApp: App {
+    var body: some Scene{
+        WindowGroup{
+            ContentView()
+        }
+    }
+}
